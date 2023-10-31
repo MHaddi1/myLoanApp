55eabad091a540be2a359497b40047fa18dc332c
Muhammad Haddi <se.m.haddi@gmail.com>
2023-10-31 13:52:37 +0500

    change in UI

--- lib/views/home/home_page.dart
+++ lib/views/home/home_page.dart
@@ -21,6 +21,13 @@ class _HomePageState extends State<HomePage> {
   final enrollController = Get.find<EnrollController>();
   final homeController = Get.put(HomeController());
   final amount = TextEditingController();
+  var statusText = "";
+  IconData iconData = Icons.home;
+  Color textColor = Colors.black;
+  String message = "";
+  IconData statusIcons = Icons.check_box;
+  bool showReSubmitButton = false;
+  bool showTextField = false;
 
   @override
   void dispose() {
@@ -40,7 +47,7 @@ class _HomePageState extends State<HomePage> {
     final userPreference = UserPreference();
 
     return Scaffold(
-      backgroundColor: Colors.blue[300],
+      backgroundColor: Colors.purple,
       appBar: AppBar(
         leading: const Icon(null),
         actions: [
@@ -94,13 +101,6 @@ class _HomePageState extends State<HomePage> {
             print(loanStatus);
           }
 
-          var statusText = "";
-          IconData iconData = Icons.home;
-          Color textColor = Colors.black;
-          String message = "";
-          IconData statusIcons = Icons.check_box;
-          bool showReSubmitButton = false;
-          bool showTextField = false;
           //bool showSubmitButton = false;
 
           if (loanStatus == 0) {
@@ -129,114 +129,257 @@ class _HomePageState extends State<HomePage> {
             showReSubmitButton = true;
           }
 
-          return Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 15),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Card(
-                  elevation: 3,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(12),
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        // Icon(
+                        //   Icons.favorite,
+                        //   color: Colors.blue,
+                        // ),
+                        // Icon(
+                        //   Icons.more_vert,
+                        //   color: Colors.blue,
+                        // ),
+                      ],
                     ),
-                    height: Get.height * 0.20,
-                    width: Get.width,
-                    child: Column(
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        iconData,
+                        color: textColor,
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        "Loan Status: $statusText",
+                        style:
+                            const TextStyle(fontSize: 20, color: Colors.white),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                    child: Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
+                        Text(
+                          message,
+                          style: const TextStyle(
+                              fontSize: 15, color: Colors.white),
+                        ),
+                        const SizedBox(
+                          width: 7,
+                        ),
+                        Icon(
+                          statusIcons,
+                          color: textColor,
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Container(
+                alignment: Alignment.bottomCenter,
+                height: MediaQuery.of(context).size.height * 0.67,
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(35),
+                      topLeft: Radius.circular(35),
+                      bottomLeft: Radius.circular(12),
+                      bottomRight: Radius.circular(12)),
+                ),
+                child: Container(
+                  decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(12),
+                          topRight: Radius.circular(12))),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
                         const Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 10),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 15, vertical: 15),
+                          child: Column(
                             children: [
-                              Icon(
-                                Icons.favorite,
-                                color: Colors.blue,
+                              MyCardDetail(),
+                              SizedBox(
+                                height: 20,
                               ),
-                              Icon(
-                                Icons.more_vert,
-                                color: Colors.blue,
+                              MyCardDetail(),
+                              SizedBox(
+                                height: 20,
                               ),
+                              MyCardDetail()
                             ],
                           ),
                         ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(
-                              iconData,
-                              color: textColor,
+                        if (showTextField)
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 15, vertical: 10),
+                            child: MyButton(
+                              text: "Request For Loan",
+                              onTap: () {
+                                Get.toNamed(RoutesName.loanApplied);
+                              },
                             ),
-                            const SizedBox(
-                              width: 10,
-                            ),
-                            Text(
-                              "Loan Status: $statusText",
-                              style: TextStyle(fontSize: 20, color: textColor),
-                            ),
-                          ],
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 10, vertical: 5),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                message,
-                                style: const TextStyle(fontSize: 15),
-                              ),
-                              const SizedBox(
-                                width: 7,
-                              ),
-                              Icon(
-                                statusIcons,
-                                color: textColor,
-                              )
-                            ],
                           ),
-                        ),
+                        if (showReSubmitButton)
+                          MyButton(
+                            text: newStatus == null
+                                ? "ReSubmit Application"
+                                : "Loan ReSubmit Application",
+                            onTap: newStatus == null
+                                ? () {
+                                    Get.toNamed(RoutesName.enrollScrren1);
+                                  }
+                                : () {
+                                    if (kDebugMode) {
+                                      //homeController.fetchData();
+                                      Get.toNamed(RoutesName.loanApplied);
+                                    }
+                                  },
+                          ),
                       ],
                     ),
                   ),
                 ),
-                const SizedBox(
-                  height: 10,
-                ),
-                if (showTextField)
-                  MyButton(
-                    text: "Request For Loan",
-                    onTap: () {
-                      Get.toNamed(RoutesName.loanApplied);
-                    },
-                  ),
-                if (showReSubmitButton)
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      MyButton(
-                        text: newStatus == null
-                            ? "ReSubmit Application"
-                            : "Loan ReSubmit Application",
-                        onTap: newStatus == null
-                            ? () {
-                                Get.toNamed(RoutesName.enrollScrren1);
-                              }
-                            : () {
-                                if (kDebugMode) {
-                                  //homeController.fetchData();
-                                  Get.toNamed(RoutesName.loanApplied);
-                                }
-                              },
-                      ),
-                    ],
-                  ),
-              ],
-            ),
+              ),
+              const Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [],
+              ),
+            ],
           );
         },
+      ),
+    );
+  }
+}
+
+class MyCardDetail extends StatelessWidget {
+  const MyCardDetail({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 12),
+      height: 200,
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(12),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              spreadRadius: 3,
+              blurRadius: 2,
+            )
+          ]),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                "Name",
+                style: TextStyle(fontSize: 25),
+              ),
+              Row(
+                children: [
+                  const Icon(
+                    Icons.delete,
+                    color: Colors.pink,
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Container(
+                      decoration: BoxDecoration(
+                          color: Colors.grey[200],
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.1),
+                              spreadRadius: 2,
+                            )
+                          ],
+                          borderRadius: BorderRadius.circular(2)),
+                      child: const Icon(Icons.settings))
+                ],
+              )
+            ],
+          ),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  Text(
+                    "\$90000",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    "90000",
+                    style: TextStyle(fontSize: 15),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Text(
+                    "5%",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    "interst",
+                    style: TextStyle(fontSize: 15),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Text(
+                    "\$390",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    "90000",
+                    style: TextStyle(fontSize: 15),
+                  ),
+                ],
+              )
+            ],
+          ),
+          const Align(
+            alignment: Alignment.topLeft,
+            child: Text(
+              "Company Name",
+            ),
+          ),
+        ],
       ),
     );
   }
